1534807e28b7dfce6ba38360b1c817163bf212ff
Alexscdt <alex.schmidt59@icloud.com>
2024-11-08 16:59:49 +0100

    feat: decode .torrent

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+import bencodepy
+
 def main():
-    print("Hello, World!")
+    with open('../torrentTest/puppy.torrent', 'rb') as file:
+        torrent = bencodepy.decode(file.read())
+        print(torrent[b'announce'].decode('utf-8'))
 
 if __name__ == "__main__":
     main()
